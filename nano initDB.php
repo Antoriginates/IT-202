89631abf33159cd4d9975fd1c707838e25f5c7ef
Antoriginates <55147489+Antoriginates@users.noreply.github.com>
2019-09-17 10:46:01 -0400

    Update nano initDB.php

--- nano initDB.php
+++ nano initDB.php
@@ -1,17 +1,6 @@
 <?php
-ini_set('display_errors',1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//TODO add error handling
+
+//load the config from the same directory
 require('config.php');
-echo "Loaded Host" . $Host;
-$conn_string = "mysql:host=$host;dbname=$database; charset=utf8mb4";
-try{
-      $db = new POD($conn_string, $username, $password):
-      echp "Connected";
-      
-}
-catch(Exception $e){
-        echo $e->getMessage();
-        exit ("Somethings Wrong");
-}
-?>
+echo "Loaded host: " . $host;
