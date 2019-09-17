11d850560e859398095ddd11b4a300c3407a59fa
Antoriginates <55147489+Antoriginates@users.noreply.github.com>
2019-09-17 10:41:47 -0400

    Update initDB.php

--- initDB.php
+++ initDB.php
@@ -1,19 +1,5 @@
 <?php
-ini_set('display_errors',1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-require('config.php');
-echo "Loaded Host" . $Host;
+require ('config.php');
 
-$conn_string = "mysql:host=$host;dbname=$database; charset=utf8mb4";
-
-try{
-      $db = new POD($conn_string, $username, $password):
-      echp "Connected";
-      
-}
-catch(Exception $e){
-        echo $e->getMessage();
-        exit ("Somethings Wrong");
-}
+Echo $host;
 ?>
