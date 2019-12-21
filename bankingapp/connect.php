9e2b7b871fc0a652b711850960670b7aee81994f
Anthony J Odige <ajo25@afsconnect1.njit.edu>
2019-12-20 23:13:19 -0500

    project connect

---
+++ bankingapp/connect.php
@@ -0,0 +1,18 @@
+<?php
+
+ini_set('display_errors',1);
+ini_set('display_startup_errors', 1);
+
+error_reporting(E_ALL);
+require('config.php');
+
+
+$connection_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+try{
+	$db = new PDO($connection_string, $username, $password);
+}
+catch(Exception $e){
+	echo $e->getMessage();
+	exit("Try Again");
+}
+?>
